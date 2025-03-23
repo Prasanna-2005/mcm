369ff0bdcfc73814e7d30550839ed10e12ae2f97
Srihariram A <harigokulvidhya@gmail.com>
2025-03-23 16:18:39 +0000

    feat: added delete review functionality

--- routes/review.js
+++ routes/review.js
@@ -1,8 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authController = require('../controllers/auth');
-const MovieFilterController = require('../controllers/review');
+const authController = require("../controllers/auth");
+const MovieFilterController = require("../controllers/review");
 
+router.get(
+  "/movies/:movieId/reviews",
+  authController.isAuthenticated,
+  MovieFilterController.getReviewsByMovieId
+);
 
+router.post(
+  "/movies/:movieId/reviews",
+  authController.isAuthenticated,
+  MovieFilterController.addReview
+);
+
+router.post(
+  "/movies/:movieId/reviews/:reviewId/:action", // action can be 'like' or 'dislike'
+  authController.isAuthenticated,
+  MovieFilterController.likeOrDislikeReview
+);
+
+router.delete(
+  "/movies/:movieId/reviews/:reviewId",
+  authController.isAuthenticated,
+  MovieFilterController.deleteReview
+);
 
 module.exports = router;
