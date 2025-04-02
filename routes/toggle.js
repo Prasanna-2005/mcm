beaf1d1b635503c283a4de81d8c7f45d7fec5a7d
Prasanna <mpk19052005@gmail.com>
2025-04-02 11:38:04 +0530

    fix..

--- routes/toggle.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/auth');
-const toggleController = require('../controllers/toggle');
-
-router.patch('/movie/toggle-like', authController.isAuthenticated, toggleController.toggleLike);
-router.patch('/movie/toggle-watchlist', authController.isAuthenticated, toggleController.toggleWatchlist);
-
-module.exports = router;
