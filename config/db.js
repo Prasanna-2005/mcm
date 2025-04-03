e78279812b9f67bcb4a9b0b1b16da1a6a4597b8d
Prasanna <mpk19052005@gmail.com>
2025-04-03 15:46:25 +0530

    HOST

--- config/db.js
+++ config/db.js
@@ -1,14 +1,10 @@
 const mysql = require("mysql2/promise");
-console.log("DB_HOST:", process.env.db_HOST);
-console.log("DB_PORT:", process.env.db_PORT);
-
 const pool = mysql.createPool({
   host: process.env.db_HOST,
   user: process.env.db_USER,
   port:process.env.db_PORT,
   password: process.env.db_PASSWORD,
-  database: process.env.db_NAME,
-  waitForConnections: true
+  database: process.env.db_NAME
 });
 
 module.exports = pool;
